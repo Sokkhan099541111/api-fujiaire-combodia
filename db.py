8d49c6b84cc6d05a2f74ec6138a4145acd273f6a
Sokkhan099541111 <sokkhan.chhon@gmail.com>
2025-11-02 12:56:13 +0700

    Update db.py
    Update password

--- db.py
+++ db.py
@@ -12,7 +12,7 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
             user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
+            password=os.getenv("DB_PASS"),
             database=os.getenv("DB_NAME"),
             auth_plugin='mysql_native_password'
         )
