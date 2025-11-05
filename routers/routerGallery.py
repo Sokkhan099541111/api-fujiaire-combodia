b31a52c59b21fd8c29cc866b84c3ca188c1ba7da
Sokkhan099541111 <sokkhan.chhon@gmail.com>
2025-11-05 16:39:23 +0700

    fix gallery router

--- routers/routerGallery.py
+++ routers/routerGallery.py
@@ -10,17 +10,17 @@ async def create_gallery(
     user_id: int = Form(...),
     image_id: int = Form(None)
 ):
-    return controllerGallery.create_gallery(file, user_id, image_id)
+    return await controllerGallery.create_gallery(file, user_id, image_id)
 
 # GET ALL ACTIVE GALLERY
 @router.get("/")
-def get_all():
-    return controllerGallery.get_all_gallery()
+async def get_all():
+    return await controllerGallery.get_all_gallery()
 
 # GET BY ID
 @router.get("/{gallery_id}")
-def get_by_id(gallery_id: int):
-    return controllerGallery.get_gallery_by_id(gallery_id)
+async def get_by_id(gallery_id: int):
+    return await controllerGallery.get_gallery_by_id(gallery_id)
 
 # UPDATE GALLERY (expects Form data)
 @router.put("/{gallery_id}")
@@ -30,9 +30,9 @@ async def update_gallery(
     image_id: int = Form(None),
     status: int = Form(1)
 ):
-    return controllerGallery.update_gallery(gallery_id, image_id, user_id, status)
+    return await controllerGallery.update_gallery(gallery_id, image_id, user_id, status)
 
 # DELETE GALLERY
 @router.delete("/{gallery_id}")
-def delete_gallery(gallery_id: int):
-    return controllerGallery.delete_gallery(gallery_id)
+async def delete_gallery(gallery_id: int):
+    return await controllerGallery.delete_gallery(gallery_id)
